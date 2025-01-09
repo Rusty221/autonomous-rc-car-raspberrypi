0ccf4fb9386e3029e4ed8e265fb139221b3e18c2
Rusty <jakob.rustler@hof-university.de>
2025-01-09 16:06:30 +0100

    Löschen veralterter Funktionen + Implementierung Livestream

--- autonomous-rc-car/run.py
+++ autonomous-rc-car/run.py
@@ -1,25 +1,17 @@
 from src.control.client import RCClient
-from src.camera.livestream import receive_stream as receive_gstreamer_stream
-from src.camera.livestream_ffmpeg import receive_stream as receive_ffmpeg_stream
-from src.camera.livestream_rtsp import receive_stream as receive_rtsp_stream
-from src.camera.livestream_http import receive_stream as receive_http_stream
 from src.utils.config import PI_HOST, PI_CONTROL_PORT, PI_STREAM_PORT
+import time
+from src.camera.livestream import receive_stream
 
 if __name__ == "__main__":
     # Steuerbefehle senden
     client = RCClient(host=PI_HOST, port=PI_CONTROL_PORT)
-    client.send_command("servo:90")
-    client.send_command("throttle:0.1")
+    #client.send_command("servo:120")
+    #client.send_command("throttle:1")
+    #time.sleep(2)
+    #client.send_command("throttle:0.0")
+    #client.send_command("servo:90")
 
     # Livestream empfangen
-    gstreamer_url = f"udp://{PI_HOST}:{PI_STREAM_PORT}"
-    # receive_gstreamer_stream(gstreamer_url)
-
-    ffmpeg_url = f"rtsp://{PI_HOST}:{PI_STREAM_PORT}/stream"
-    receive_ffmpeg_stream(ffmpeg_url)
-
-    rtsp_url = f"rtsp://{PI_HOST}:{PI_STREAM_PORT}/stream"
-    # receive_rtsp_stream(rtsp_url)
-
-    http_url = f"http://{PI_HOST}:{PI_STREAM_PORT}/stream.mjpg"
-    # receive_http_stream(http_url)
+    motion_url = f"http://{PI_HOST}:{PI_STREAM_PORT}/"
+    receive_stream(motion_url)
--- rc-car-raspberry/src/sensors/camera.py
+++ rc-car-raspberry/src/sensors/camera.py
@@ -1,67 +1,9 @@
-# Installiere GStreamer vorher:
-# sudo apt install gstreamer1.0-tools gstreamer1.0-plugins-base gstreamer1.0-plugins-good
-
 import os
-
-def start_camera_stream_gstreamer():
-    laptop_ip = "192.168.171.119"  # Ersetze mit der IP-Adresse des Laptops
-    port = 5000
-
-    gstreamer_command = (
-        f"gst-launch-1.0 v4l2src ! "
-        f"videoconvert ! "
-        f"jpegenc ! "
-        f"rtpjpegpay ! "
-        f"udpsink host={laptop_ip} port={port}"
-    )
-
-    print("Starte GStreamer Kamera-Stream...")
-    os.system(gstreamer_command)
-
-def start_camera_stream_ffmpeg():
-    laptop_ip = "192.168.171.119"  # Ersetze mit der IP-Adresse des Laptops
-    port = 8554
-
-    ffmpeg_command = (
-        f"ffmpeg -f v4l2 -i /dev/video0 -vcodec libx264 -f rtsp rtsp://{laptop_ip}:{port}/stream"
-    )
-
-    print("Starte FFmpeg Kamera-Stream...")
-    os.system(ffmpeg_command)
-
-def start_camera_stream_rtsp():
-    laptop_ip = "192.168.171.119"  # Ersetze mit der IP-Adresse des Laptops
-    port = 8554
-
-    rtsp_command = (
-        f"gst-launch-1.0 v4l2src ! "
-        f"videoconvert ! "
-        f"x264enc tune=zerolatency ! "
-        f"rtph264pay config-interval=1 pt=96 ! "
-        f"udpsink host={laptop_ip} port={port}"
-    )
-
-    print("Starte RTSP Kamera-Stream...")
-    os.system(rtsp_command)
-
-def start_camera_stream_http():
-    laptop_ip = "192.168.171.119"  # Ersetze mit der IP-Adresse des Laptops
-    port = 8080
-
-    http_command = (
-        f"gst-launch-1.0 v4l2src ! "
-        f"videoconvert ! "
-        f"jpegenc ! "
-        f"multipartmux ! "
-        f"tcpserversink host={laptop_ip} port={port}"
-    )
-
-    print("Starte HTTP/MJPEG Kamera-Stream...")
-    os.system(http_command)
+    
+def start_camera_stream_motion():
+    cmd = "sudo systemctl start motion"
+    print("Starte Motion Kamera-Stream...")
+    os.system(cmd)
 
 if __name__ == "__main__":
-    # Wähle die gewünschte Methode zum Starten des Kamera-Streams
-    # start_camera_stream_gstreamer()
-    # start_camera_stream_ffmpeg()
-    # start_camera_stream_rtsp()
-    start_camera_stream_http()
+    start_camera_stream_motion()
