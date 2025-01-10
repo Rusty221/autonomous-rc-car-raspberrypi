84d16216695578152860fc339141433d63868866
Rusty <jakob.rustler@hof-university.de>
2025-01-10 14:09:54 +0100

    Test Remote Server Start

--- autonomous-rc-car/run.py
+++ autonomous-rc-car/run.py
@@ -6,11 +6,11 @@ from src.camera.livestream import receive_stream
 if __name__ == "__main__":
     # Steuerbefehle senden
     client = RCClient(host=PI_HOST, port=PI_CONTROL_PORT)
-    #client.send_command("servo:120")
-    #client.send_command("throttle:1")
-    #time.sleep(2)
-    #client.send_command("throttle:0.0")
-    #client.send_command("servo:90")
+    client.send_command("throttle:1")
+    client.send_command("throttle:0.1")
+    time.sleep(10)
+    client.send_command("throttle:0.0")
+    client.send_command("servo:90")
 
     # Livestream empfangen
     motion_url = f"http://{PI_HOST}:{PI_STREAM_PORT}/"
